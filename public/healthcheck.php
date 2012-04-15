4b99d63091caf81202f06a753177913691337f07
Eric Dennis <me@ericdennis.com>
2012-04-15 04:47:13 -0700

    Updated healthcheck to show redis slaves

--- public/healthcheck.php
+++ public/healthcheck.php
@@ -1,1 +1,9 @@
-<?php echo "OK"; ?>
+<?php
+require '../vendor/.composer/autoload.php';
+require '../config/settings.php';
+
+echo("OK");
+
+echo("Configured Redis slaves:");
+print_r($redis_slaves);
+?>
